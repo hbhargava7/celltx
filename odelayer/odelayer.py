3cd183190638f544c087019f58be85055b433f1e
Hersh Bhargava <hershkbhargava@gmail.com>
2020-07-15 18:12:21 -0700

    Lambdification moved to odelayer

--- odelayer/odelayer.py
+++ odelayer/odelayer.py
@@ -8,6 +8,7 @@ class ODELayer():
 
     def __init__(self, equations):
         self.equations = equations
+        self.f_model = None
 
     def ravel_expression(self, expr):
         args = []
@@ -25,11 +26,42 @@ class ODELayer():
                 if isinstance(term, Constant):
                     constants.append(term)
         return list(set(constants))
-    def model(self, x, t, p):
-        # x is an array of n variables
-        # t is the time
-        # p is a dict with the param values
-        pass
+
+    def gen_ode_model(self):
+        rhss = [i.rhs for i in model.ode.equations]
+        lhss = [i.lhs for i in model.ode.equations]
+
+        all_args = []
+        for eq in rhss:
+            args = model.ode.ravel_expression(eq)
+            for a in args:
+                all_args.append(a)
+
+        # remove duplicates
+        unique_sels = []
+        unique_consts = []
+        seen_sel = []
+        seen_const = []
+
+        for val in all_args:
+            if isinstance(val, Selector):
+                if val.selector not in seen_sel:
+                    seen_sel.append(val.selector)
+                    unique_sels.append(val)
+            elif isinstance(val, Constant):
+                if val.name not in seen_const:
+                    seen_const.append(val.name)
+                    unique_consts.append(val)
+            else:
+                print('unable to handle %s' % val)
+
+        unique_args = unique_sels + unique_consts
+
+        self.f_model = sy.lambdify(unique_args, rhss)
+
+    def model(self, X, t, args):
+        in_vals = np.concatenate((X, args))
+        return self.f_model(*in_vals)
 
     def split_parameter(self, parameter):
         pass
